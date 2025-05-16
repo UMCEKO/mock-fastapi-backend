84910da7dfaf113a4b68d895ef5b424fe405d076
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-16 21:39:22 +0300

    Move code to the app folder.

--- app/api/v1/upload.py
+++ app/api/v1/upload.py
@@ -3,8 +3,6 @@ from schemas.BaseResponse import BaseResponse
 async def upload_file():
     url: str = ""
 
-
-
     return BaseResponse(success=True, data={
         "url": url
     }, message="Successful upload.")
