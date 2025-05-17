9bb3bee619e4e649c98b9f5075267add452f4e41
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-17 16:51:11 +0300

    Add retry decorator

---
+++ app/utils/retry.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+
+def retry_async(retry_count=3):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            remaining_tries = retry_count
+            last_exception = None
+            while remaining_tries > 0:
+                try:
+                    return await func(*args, **kwargs)
+                except Exception as e:
+                    remaining_tries -= 1
+                    last_exception = e
+            raise last_exception
+        return wrapper
+    return decorator
