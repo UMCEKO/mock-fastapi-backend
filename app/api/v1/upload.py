32daf5a77d4e863a103ab1dde393a9496df5f9d8
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-17 15:26:08 +0300

    Improvements

--- app/api/v1/upload.py
+++ app/api/v1/upload.py
@@ -1,8 +1,13 @@
-import urllib
 import uuid
 
 from urllib.parse import urlparse, urlunparse
-from fastapi import APIRouter, UploadFile
+
+import fastapi
+import magic
+from fastapi import APIRouter, UploadFile, HTTPException, File
+from typing_extensions import Optional
+from magic import Magic
+from fastapi.responses import JSONResponse
 from app.core.config import S3_BUCKET, MEDIA_URL
 from app.schemas.base_response import BaseResponse
 from app.schemas.upload import UploadResponse
@@ -10,16 +15,31 @@ from app.services.s3_client import s3_client
 
 router = APIRouter(prefix="/upload", tags=["Upload"])
 
+MAX_FILE_SIZE = 10 * 1024 * 1024
 
-@router.post("", response_model=BaseResponse[UploadResponse])
-async def upload_file(file: UploadFile):
-    # Add file size checks, file type checks.
-    print(file.content_type)
+@router.post(
+    "",
+    response_model=BaseResponse[Optional[UploadResponse]],
+    response_description="The pre-signed url of the uploaded file that will last 1 hour.",
+    summary = "Upload a PDF file",
+    description = (
+        "This endpoint allows you to upload PDF documents to be processed later. "
+        "The uploaded file is stored and returned as a pre-signed URL valid for 1 hour."
+    )
+)
+async def upload_pdf(file: UploadFile = File(description="The file to be uploaded that will be sent as form data.")):
+    # File size and type validation.
+    contents = await file.read(size=MAX_FILE_SIZE + 1)
+    if len(contents) > MAX_FILE_SIZE:
+        raise HTTPException(status_code=422, detail="Failure to upload the image. The file exceeded the 10MB file size limit.")
+
+    mime_type = magic.from_buffer(contents)
+    if not mime_type.startswith("PDF document"):
+        raise HTTPException(status_code=422, detail="The uploaded document is not a pdf file.")
 
     # The file url to be uploaded/accessed
     key = f"uploads/{uuid.uuid4().hex}"
 
-    contents = await file.read()
     # Upload the file
     s3_client.put_object(
         Bucket=S3_BUCKET,
@@ -28,11 +48,14 @@ async def upload_file(file: UploadFile):
         ContentType=file.content_type
     )
 
+    # Generate a pre-signed url for security purposes.
     url = s3_client.generate_presigned_url("get_object", Params={
         "Bucket": S3_BUCKET,
         "Key": key,
     })
 
+
+    # Safely parse and unparse the url in order to avoid injection attacks.
     parsed = urlparse(url)
     custom_url = urlunparse((
         MEDIA_URL.scheme,
@@ -43,4 +66,4 @@ async def upload_file(file: UploadFile):
         parsed.fragment
     ))
 
-    return BaseResponse(success=True, data=UploadResponse(url=str(custom_url)), message="Successful upload.")
+    return BaseResponse(success=True, data=UploadResponse(url=str(custom_url)), message=f"Successful upload.")
